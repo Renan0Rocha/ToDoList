a7168b59daf65d13cceb4d6efc1925b931e3f0af
Renan Rocha <102130210+Renan0Rocha@users.noreply.github.com>
2023-06-10 17:35:51 -0400

    organizar pastas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,18 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+
+      <div>
+        TESTE
+      </div>
+
+
+
+
+
+
+
+      {/* <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
@@ -89,7 +100,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
